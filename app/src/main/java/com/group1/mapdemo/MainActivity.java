360c788a8dab46b396f0d2dce170bc80b535ec45
Ngô Gia Thưởng <110913482+giathuong172@users.noreply.github.com>
2023-06-19 09:17:35 +0700

    [Thuong] add some feature (#2)

--- app/src/main/java/com/group1/mapdemo/MainActivity.java
+++ app/src/main/java/com/group1/mapdemo/MainActivity.java
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 // Import other libraries
+import android.view.View;
 import android.view.WindowManager;
 import android.Manifest;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 // Import google map libaries
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -29,10 +34,16 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
+import java.io.IOException;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    GoogleMap mMap;
     private FusedLocationProviderClient fusedLocationProviderClient;
     private SupportMapFragment mapFragment;
+    Button btnZoomIn,btnZoomOut,btnSearch,btnCurrent;
+    EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,11 +71,59 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
                     @Override
                     public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
-
+                            permissionToken.continuePermissionRequest();
                     }
-                });
+                }).check();
 
         mapFragment.getMapAsync(this);
+        btnZoomIn = (Button) findViewById(R.id.zoomin);
+        btnZoomOut =(Button) findViewById(R.id.zoomout);
+        btnZoomIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMap.moveCamera(CameraUpdateFactory.zoomIn());
+            }
+        });
+        btnZoomOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMap.moveCamera(CameraUpdateFactory.zoomOut());
+            }
+        });
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+        et = (EditText) findViewById(R.id.et_location);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMap.clear();
+                String location = et.getText().toString();
+                List<Address> addressList;
+                MarkerOptions mo= new MarkerOptions();
+                if(!location.trim().equals("")){
+                    Geocoder geocoder = new Geocoder(MainActivity.this);
+                    try {
+                        addressList = geocoder.getFromLocationName(location,5);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    for (int i=0;i<addressList.size();i++){
+                        Address myAddress = addressList.get(i);
+                        LatLng latLng = new LatLng(myAddress.getLatitude(),myAddress.getLongitude());
+                        mo.position(latLng);
+                        mo.title("Your search result");
+                        mMap.addMarker(mo);
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                    }
+                }
+            }
+        });
+        btnCurrent = (Button) findViewById(R.id.btn_current);
+        btnCurrent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 getCurrentLocation();
+            }
+        });
     }
 
     public void getCurrentLocation() {
@@ -101,6 +160,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
         // Use the GoogleMap object to perform map-related operations
         // For example, you can add markers, set camera position, etc.
         // Refer to the Google Maps Android API documentation for more details.
